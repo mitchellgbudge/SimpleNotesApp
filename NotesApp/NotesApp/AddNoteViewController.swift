f855c2200a73ef02eb2340563f68ed6144ff8302
Mitchell Budge <mitchellbudge@gmail.com>
2019-08-15 14:34:01 -0600

    Set up add note UI

---
+++ NotesApp/NotesApp/AddNoteViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddNoteViewController.swift
+//  NotesApp
+//
+//  Created by Mitchell Budge on 8/15/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
