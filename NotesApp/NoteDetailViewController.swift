8aaad1f4ae710463a7fe6f91073837fa96e0bc87
William Chen <chen.will7@gmail.com>
2019-08-15 13:44:03 -0700

    Set up detail controller and wired up

---
+++ NotesApp/NoteDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NoteDetailViewController.swift
+//  NotesApp
+//
+//  Created by William Chen on 8/15/19.
+//  Copyright © 2019 William Chen. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var textField: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
