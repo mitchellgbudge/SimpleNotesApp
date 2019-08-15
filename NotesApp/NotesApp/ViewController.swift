8583d6a00790b323fe2d4958578388fc98a53922
Mitchell Budge <mitchellbudge@gmail.com>
2019-08-15 13:53:50 -0600

    Created initial Xcode project

---
+++ NotesApp/NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Mitchell Budge on 8/15/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
